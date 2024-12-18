e6505ebd3af8990749aff7a0fca19bfc4275b02d
Chaoran Zhou <zhouchaoran304@gmail.com>
2024-12-18 14:37:24 -0500

    Dec, 18

--- finetuning/finetuning_apt.py
+++ finetuning/finetuning_apt.py
@@ -1,7 +1,7 @@
 import time
 
 out_dir = 'out-apt'
-eval_interval = 5
+eval_interval = 7
 eval_iters = 40
 wandb_log = False # feel free to turn on
 wandb_project = 'shakespeare'
@@ -15,10 +15,12 @@ always_save_checkpoint = False
 
 # the number of examples per iter:
 # 1 batch_size * 32 grad_accum * 1024 tokens = 32,768 tokens/iter
-# shakespeare has 301,966 tokens, so 1 epoch ~= 9.2 iters
+# Adjusted for dataset with ~945k training tokens
+# Current setup: 32,768 tokens/iter means ~29 iters/epoch
+# Modified for 3-5 epochs of training
 batch_size = 1
 gradient_accumulation_steps = 32
-max_iters = 20
+max_iters = 100 # ~3.5 epochs
 
 # finetune at constant LR
 learning_rate = 3e-5
